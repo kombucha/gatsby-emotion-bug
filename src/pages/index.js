7b36f515e57ee86f9d881851b1407010c5ddb91b
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2019-08-11 13:29:17 +0200

    Setup example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import { css, CacheProvider } from "@emotion/core"
+import createCache from "@emotion/cache"
 
-export default () => <div>Hello world!</div>
+// This one will work
+// const myCache = createCache({ key: "stl" })
+
+const MyWrapper = ({ children, key = "stl" }) => {
+  // This one won't work (if you comment out the other one)
+  const myCache = createCache({ key })
+  return <CacheProvider value={myCache}>{children}</CacheProvider>
+}
+
+export default () => (
+  <MyWrapper>
+    <div
+      css={css`
+        color: green;
+      `}
+    >
+      Hello world!
+    </div>
+  </MyWrapper>
+)
